24dcd2496bb3064fd5d71477ee6d237754fe6a18
Priyanshi Kevadiya <priyanshikevadiya222@gmail.com>
2022-08-05 10:40:42 +0530

    first commit

---
+++ tmp/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace tmp.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
